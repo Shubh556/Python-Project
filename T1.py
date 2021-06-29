952c1d953049a240c7daf786ac751a28f5feca15
Shubham Lotwala <84775410+Shubh556@users.noreply.github.com>
2021-06-29 19:30:53 +0530

    Delete T1.py

--- T1.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-root=Tk()
-root.geometry("300x300")
-root.title("GUI")
-lab=Label(text="Ready",bg="yellow",fg="green")
-lab.pack(side=BOTTOM,anchor="se",fill=X)
-lab.pack()
-root.mainloop()
