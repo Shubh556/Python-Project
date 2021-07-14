95182922e6fd32fa25865b0ff9501164e46f63d0
Shubham Lotwala <84775410+Shubh556@users.noreply.github.com>
2021-07-14 15:39:21 +0530

    Add files via upload

---
+++ automateshutdown.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+pyautogui.FAILSAFE = False
+# while True:
+pyautogui.moveTo(0,800)
+pyautogui.click(button='left')
+pyautogui.moveTo(20,700)
+pyautogui.click(button='right')
+pyautogui.moveTo(20,628)
+pyautogui.press('enter')
+                             
