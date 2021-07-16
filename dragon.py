4597690aad8e153e7be2f05f0cfa0d29c3c7454d
Shubham Lotwala <84775410+Shubh556@users.noreply.github.com>
2021-07-16 11:20:57 +0530

    Add files via upload

--- dragon.py
+++ dragon.py
@@ -1,12 +1,14 @@
 import pyautogui
 import time
 pyautogui.FAILSAFE = False
+pyautogui.hotkey('alt', 'f4')
+pyautogui.hotkey('alt', 'f4')
 # while True:
-pyautogui.moveTo(0,800)
-pyautogui.click(button='left')
-pyautogui.moveTo(20,700)
-pyautogui.click(button='right')
-pyautogui.moveTo(20,628)
-pyautogui.click(button='left')
-pyautogui.click(button='left')
+# pyautogui.moveTo(0,800)
+# pyautogui.click(button='left')
+# pyautogui.moveTo(20,700)
+# pyautogui.click(button='right')
+# pyautogui.moveTo(20,628)
+# pyautogui.click(button='left')
+# pyautogui.click(button='left')
                              
