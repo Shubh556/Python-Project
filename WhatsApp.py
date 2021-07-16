aa39fcd11045f814f611715e5e3bcd0fc170915b
Shubham Lotwala <84775410+Shubh556@users.noreply.github.com>
2021-07-16 19:34:54 +0530

    Add files via upload

---
+++ WhatsApp.py
@@ -0,0 +1,7 @@
+import pywhatkit as kit
+import time
+time.sleep(2)
+kit.playonyt("safarnama")
+kit.sendwhatmsg_instantly("+91{1234567890}","HI\nThis message is send using python interpretor",18,57)
+kit.image_to_ascii_art(r"C:\Users\user\Downloads\2.jpg",r"C:\Users\user\Downloads\2.txt")
+print("The Photo has been saved")
